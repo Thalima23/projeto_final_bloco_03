3a5c434f2cdb32c6448ee9290421a6c190c188fc
Thalitalima23 <thalitalima23@gmail.com>
2025-12-02 10:07:03 -0300

    :sparkles: Atualizando Rotas e o vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
-function App() {
-	return (
-		<>
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
 
-		</>
-	)
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        {/* ROTA PRINCIPAL */}
+        <Route path="/" element={<Home />} />
+
+        {/* ROTA /home */}
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
