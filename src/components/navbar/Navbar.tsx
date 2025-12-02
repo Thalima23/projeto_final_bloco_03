eef0003090260c487cc65aaba9db62f0ff7dc6b5
Thalitalima23 <thalitalima23@gmail.com>
2025-12-02 09:53:36 -0300

    :sparkles: Adicionando componentes

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom"
+
+function Navbar() {
+    return (
+        <>
+            <div className='w-full flex justify-center py-4 
+    bg-green-700 text-white'>
+
+                <div className="container flex justify-between text-lg mx-12">
+                    <Link to='/home' className="text-2xl font-bold">PrimeFarma</Link>
+                    
+
+
+                    <div className='flex gap-4'>
+                        <Link to='/home'className='hover:underline'>Home</Link>
+                        <Link to='/produto' className='hover:underline'>Produtos</Link>
+                        <Link to='/categorias' className='hover:underline'>Categorias</Link>
+
+
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Navbar
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,34 @@
+function Home() {
+  return (
+    <>
+      {/* Container geral */}
+      <div className="bg-green-800 flex justify-center py-10">
+
+        {/* GRID — As duas colunas ficam DENTRO dele */}
+        <div className="container grid grid-cols-1 md:grid-cols-2 text-white items-center gap-8">
+
+          {/* COLUNA ESQUERDA */}
+          <div className="flex flex-col gap-4 items-center md:items-start px-4">
+            <h2 className="text-5xl font-bold">Seja Bem Vinde! à PrimeFarma</h2>
+
+            <p className="text-xl">
+              Cuidando da sua saúde com confiança e qualidade.
+            </p>
+          </div> {/* <-- ESTA DIV ESTAVA FALTANDO FECHAR */}
+
+          {/* COLUNA DIREITA */}
+          <div className="flex justify-center">
+            <img
+              src="https://ik.imagekit.io/Thalima23/Copilot_20251121_110050.png?updatedAt=1763733676881"
+              alt="Imagem da Página home"
+              className="w-2/3"
+            />
+          </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
