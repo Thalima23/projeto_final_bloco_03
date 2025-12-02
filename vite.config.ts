012fe3b119b9a813cb2b87f51895c1cebaf0799e
Thalitalima23 <thalitalima23@gmail.com>
2025-12-02 15:36:43 -0300

    Fix Vite base path for Vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from "@tailwindcss/vite";
-
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/projeto_final_bloco_03/',
-  plugins: [
-    react(),
-    tailwindcss()
-  ]
+  plugins:[
+   react(),
+tailwindcss()
+],
 })
-
-
